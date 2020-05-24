1652e8faafb08a392aacaec0f1da168b0f06b71d
Hrdas <hrdyjan1@gmail.com>
2020-05-24 20:07:27 +0200

    First dummy test

---
+++ react-typescript/src/__tests__/function.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { enhanceItemArray } from '../helpers/functions';
+
+describe('Helpers', () => {
+  test('Enhance Item Array', () => {
+    const emptyArray = [];
+
+    expect(1).toEqual(1);
+  });
+});
